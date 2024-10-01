f92bb0d7d5f8560af1e5dd902a725430b0e56683
Anna-Babich <anbabi@yandex.ru>
2024-10-01 12:37:18 +0300

    Добавление и удаление карточек js

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,39 @@
 // @todo: Темплейт карточки
 
+const cardTemplate = document.querySelector('#card-template').content;
+
 // @todo: DOM узлы
+
+const deleteButton = cardTemplate.querySelector('.card__delete-button');
+const placesList = document.querySelector('.places__list');
 
 // @todo: Функция создания карточки
 
+function createCard (link, name, buttonDelete) {
+    const cardContent = cardTemplate.querySelector('.card');
+    cardTemplate.querySelector('.card__image').src = link;
+    cardTemplate.querySelector('.card__title').textContent = name;
+
+    addEventListener('click', buttonDelete)
+
+    return cardContent;
+}
+
 // @todo: Функция удаления карточки
 
+function deleteBtn (event) {
+    const deleteItem = event.target.closest('.card');
+    deleteItem.remove();
+}
+
 // @todo: Вывести карточки на страницу
+
+function renderCard (array) {
+    array.forEach(function(item) {
+        const card = createCard(item.link, item.name, deleteBtn).cloneNode(true);
+        placesList.append(card);
+    })
+}
+
+renderCard(initialCards);
+
